a5904b02c23e876ea868d9b7ae69a71e20f6312f
jiesinlim <71651976+jiesinlim@users.noreply.github.com>
2021-10-23 04:52:00 +0800

    updated backend logic to enrol into first class with vacancy
    also added new class_name column to enrolled_courses table

--- LMS/backend/addEnrolledCourses.php
+++ LMS/backend/addEnrolledCourses.php
@@ -1,12 +1,13 @@
 <?php
     require_once('common.php');
-    $enrolledCoursesDAO = new enrolledCoursesDAO;
+    $enrolledCoursesDAO = new EnrolledCoursesDAO;
     $user_id = (int)$_GET['userId'];
     $user_name = $_GET['userName'];
     $course_id = (int)$_GET['courseId'];
     $course_name = $_GET['courseName'];
+    $class_name = $_GET['className'];
     
-    $status = $enrolledCoursesDAO->addEnrolledCourses($user_id, $user_name, $course_id, $course_name);
+    $status = $enrolledCoursesDAO->addEnrolledCourses($user_id, $user_name, $course_id, $course_name, $class_name);
     
     if($status){
         echo 'true';
--- LMS/lmsClasses/ClassesDAO.php
+++ LMS/lmsClasses/ClassesDAO.php
@@ -1,19 +1,20 @@
 <?php
 require_once 'autoload.php';
 
-class CoursesDAO {
-    public function getClasses() {
+class ClassesDAO {
+    public function getClassesWithVacancy($course_id) {
         $connMgr = new ConnectionManager();
         $pdo = $connMgr->getConnection();
 
-        $sql = 'select * from classes';
+        $sql = 'SELECT * from classes where course_id = :course_id';
         $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':course_id', $course_id, PDO::PARAM_INT);
         $stmt->execute();
         $result = [];
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
         while($row = $stmt->fetch()) {
-            $result[] = new courses($row['course_id'], $row['course_name'], $row['class_name'], $row['start_date'], $row['end_date'], $row['slots_available'], $row['trainer_id'], $row['trainer_name']);
+            $result[] = new classes($row['course_id'], $row['course_name'], $row['class_name'], $row['start_date'], $row['end_date'], $row['slots_available'], $row['trainer_id'], $row['trainer_name']);
         }
         $stmt = null;
         $pdo = null;
