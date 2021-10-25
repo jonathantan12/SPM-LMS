d515f944f6e8ab6b0fd96c7172162d8352e95986
JunLonggggg <junlong973@gmail.com>
2021-10-25 08:23:41 +0800

    Update courseCatalogue.js

--- LMS/js/courseCatalogue.js
+++ LMS/js/courseCatalogue.js
@@ -1,11 +1,11 @@
 if(sessionStorage.getItem("user_id")){
-    let user_id = sessionStorage.getItem("user_id");
+    var user_id = sessionStorage.getItem("user_id");
 }
 else{
     sessionStorage.setItem("user_id","1");
 }
 if(sessionStorage.getItem("user_name")){
-    let user_name = sessionStorage.getItem("user_name");
+    var user_name = sessionStorage.getItem("user_name");
 }
 else{
     sessionStorage.setItem("user_name","Jonathan");
@@ -13,16 +13,16 @@ else{
 
 var request = new XMLHttpRequest()
 
-var url = "../LMS/backend/getRequiredCourses.php"
+var url = "../LMS/backend/getEngineersAndRequiredCourses.php"
 
 request.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var responce = JSON.parse(this.responseText);
-        var requiredCourses = responce;
+        var userRequiredCourses = responce[user_name]["courses"];
         var requiredCoursesHtml = document.getElementById("coursesAvailable");
-        var numRequiredCourses = requiredCourses.length;
-        if (requiredCourses) {
-            var courseUrl = "../LMS/backend/getCourses.php";
+        var numRequiredCourses = userRequiredCourses.length;
+        if (userRequiredCourses) {
+            var courseUrl = "../LMS/backend/getClasses.php";
             request.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
                     var response = JSON.parse(this.responseText);
@@ -31,7 +31,6 @@ request.onreadystatechange = function () {
                     for (var i=0; i< numCourses ;i++) {
                         courses[response[i].course_name] = {
                             "class_name": response[i].class_name,
-                            "course_desc": response[i].course_desc,
                             "course_id": response[i].course_id,
                             "end_date": response[i].end_date,
                             "start_date": response[i].start_date,
@@ -45,10 +44,10 @@ request.onreadystatechange = function () {
                                 <div class="card" style="width: 20rem;">
                                 <a href="#"><img src="assets/placeholder_img.png" class="card-img-top" alt="..."></a>
                                     <div class="card-body">
-                                        <h5 class="card-title text-center">${requiredCourses[x].course_name}<br>(Required)</h5>
-                                        <p class="card-text">Class size: <span>${courses[requiredCourses[x].course_name].slots_available}</span></p>
-                                        <p class="card-text">Duration: <span>${courses[requiredCourses[x].course_name].start_date} - ${courses[requiredCourses[x].course_name].end_date}</span></p>
-                                        <a href="enrol.html?courseId=${requiredCourses[x].required_course_id}" class="btn btn-outline-success" id="enrol ${requiredCourses[x].course_name}">Enrol</a>
+                                        <h5 class="card-title text-center">${userRequiredCourses[x].course_name}<br>(Required)</h5>
+                                        <p class="card-text">Class size: <span>${courses[userRequiredCourses[x].course_name].slots_available}</span></p>
+                                        <p class="card-text">Duration: <span>${courses[userRequiredCourses[x].course_name].start_date} - ${courses[userRequiredCourses[x].course_name].end_date}</span></p>
+                                        <a href="enrol.html?courseId=${userRequiredCourses[x].course_id}" class="btn btn-outline-success" id="enrol ${userRequiredCourses[x].course_name}">Enrol</a>
                                     </div>
                                 </div>
                             </div> `;
