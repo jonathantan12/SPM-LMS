bab66c08f782d236a4200b40033da781616d105a
claireoxxx <claire.ong.2019@smu.edu.sg>
2021-11-06 19:23:57 +0800

    changes

--- LMS/lmsClasses/dbScript.sql
+++ LMS/lmsClasses/dbScript.sql
@@ -281,14 +281,6 @@ CREATE TABLE IF NOT EXISTS `quizzes` (
 
 ALTER TABLE quizzes ADD CONSTRAINT PK_Quiz PRIMARY KEY (course_id,course_class_id,section_id,quiz_id,question_no);
 --
--- Dumping data for table `quiz_answers`
---
-
-INSERT INTO `quizzes` (`course_id`,`course_class_id`,`section_id`,`quiz_id`,`quiz_title`,`quiz_type`, `question_no`, `question`, `number_of_options`, `options_content` ,`correct_answer`) VALUES
-(2, 1, 1, 1,'quiz title 1','graded',1,'Is it true that it is like this?', 2, "['true','false']",'true'),
-(2, 1, 1, 1,'guiz title 2','ungraded',2,'Is it true that it is like this?', 2, "['true','false']",'false'),
-(2, 1, 1, 1,'quiz title 3','graded',3,'Is it true that it is like this?', 2, "['true','false']",'false');
-COMMIT;
 
 
 
